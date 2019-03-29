5cdfc22fec82679937ec2a5742edbe1b833d17ba
Jeong Yeon Cho <chodr@bc.edu>
2019-03-28 20:25:00 -0400

    cardPanel implemented as class component

--- src/panels/cardPanel.jsx
+++ src/panels/cardPanel.jsx
@@ -1,19 +1,87 @@
 import React, { Component } from 'react'
 import { Card } from '../listItems.js'
 
-function CardPanel(props) {
-  let cards
-  console.log(props.listItems.items);
-  if (props.listItems.items.length === 0) {
-    cards = (<h1>Nothing to see here</h1>)
-  } else {
-    cards = props.listItems.items.map(card => {
-      return (
-        <Card answered={card.answered} key={card.id} id={card.id} timeCreated={card.timeCreated} question={card.question} body={card.body} />
-      )
+class CardPanel extends Component {
+  constructor(props) {
+    super(props)
+    this.handleChange = this.handleChange.bind(this)
+
+    this.state = {
+      query: "",
+      items: [] ,
+      internal: false,
+    }
+  }
+  static getDerivedStateFromProps(props, state) {
+    if ( state.internal === true ) {  //  ignore if internal update to state
+      return {internal:false}
+    }
+    // set props as state if its not an internal update
+    return {
+      items: props.listItems.items,  //  set any updates
+      query: '',                     //  reset query
+      internal: false                // set interal update to false just in case
+    }
+  }
+
+  searchTags(query) {
+    let result = this.props.listItems.items
+      .slice()
+      .filter(function(item) {
+        let values = Object.values(item)
+
+        for (let value of values) {
+          if (String(value).toLowerCase().includes(query.toLowerCase())) {
+            return true
+          }
+        }
+        return false
+      })
+    return result
+  }
+
+  handleChange(e){
+    let query = e.target.value.trim()
+    let items = this.searchTags(query)
+
+    console.log(items);
+    this.setState({
+      query: e.target.value,
+      items: items,
+      internal: true
     })
   }
-  return (<div id="panel1">{props.type} {cards}</div>)
+
+  render() {
+
+    let title = "Cards"
+    let subTitle = "Click a tag to see comments and answer"
+
+    let list = this.state.items
+
+    console.log(list);
+    if (list.length === 0) {
+      list = (<h1>Nothing</h1>)
+    } else {
+      list = list.map((card) => <Card className="row card" answered={card.answered} key={card.id} id={card.id} timeCreated={card.timeCreated} question={card.question} body={card.body} />)
+    }
+
+    // <div id="panel1">{this.props.type} {list}</div>)
+    return (
+      <span id={this.props.type + "Panel"} className="bg-primary card">
+        <h1 className="heading">{title}</h1>
+        <p>{subTitle}<br />{this.state.state}</p>
+        <div className="form-inline justify-content-center p-1">
+          <input type="text" className="form-control" placeholder="Search" id="searchCards" value={this.state.query} onChange={this.handleChange}/>
+        </div>
+        <div id={this.props.type + "List"}>
+          {list}
+        </div>
+      </span>
+
+    )
+  }
 }
+
 
 export default CardPanel
