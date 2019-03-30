1abbd905e842d2b4b0299993b9ae04ce7a6dbe6f
Jeong Yeon Cho <chodr@bc.edu>
2019-03-30 00:26:50 -0400

    removed firebase init from panelGroup.js

--- src/panelGroup.js
+++ src/panelGroup.js
@@ -5,19 +5,6 @@ import CommentPanel from "./panels/commentPanel.js"
 import db from "./firestore.js"
 
 
-// initialize firebase
-var config = {
-    apiKey: "AIzaSyACuA8CM5fKm59JMMeE72gNadh6yVCdyvc",
-    authDomain: "cards-5c8be.firebaseapp.com",
-    databaseURL: "https://cards-5c8be.firebaseio.com",
-    projectId: "cards-5c8be",
-    storageBucket: "cards-5c8be.appspot.com",
-    messagingSenderId: "222433486904"
-  };
-const firebase = require("firebase")
-require("firebase/firestore")
-firebase.initializeApp(config)
-let db = firebase.firestore()
 
 class PanelGroup extends Component {
   constructor(props) {
